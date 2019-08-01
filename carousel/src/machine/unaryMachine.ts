913acbe7698bca14b66c86b5776603d43b05f0e5
Farzad YZ <farskid@gmail.com>
2019-08-01 20:59:13 +0300

    cleanup unnecessary stuff from unaryMachine

--- carousel/src/machine/unaryMachine.ts
+++ carousel/src/machine/unaryMachine.ts
@@ -1,28 +1,11 @@
-import {
-  CarouselContext,
-  UnaryCarouselStateSchema,
-  CarouselEvent,
-  HeadlessCarouselProps,
-} from "../types";
+import { UnaryCarouselStateSchema } from "../types";
 
 import { Machine } from "xstate";
-import { constructGroups } from "../utils";
 
-export function unaryCarouselMachine(config: HeadlessCarouselProps) {
-  const { startIndex, slidesToShow, totalItems, dir, infinite } = config;
-  const groups = constructGroups({ totalItems, slidesToShow, startIndex });
-  return Machine<CarouselContext, UnaryCarouselStateSchema, CarouselEvent>({
+export function unaryCarouselMachine() {
+  return Machine<undefined, UnaryCarouselStateSchema>({
     id: "unaryCarousel",
     initial: "running",
-    context: {
-      cursor: 1,
-      min: 1,
-      max: totalItems,
-      dir,
-      infinite,
-      slidesToShow,
-      groups,
-    },
     states: {
       running: {},
     },
